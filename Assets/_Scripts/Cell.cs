bbd1efb880d6cf78d3ca8b2d2f647ac74dfa63a0
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-02-15 10:15:30 +0200

    Started creating a state logic for cell

--- Assets/_Scripts/Cell.cs
+++ Assets/_Scripts/Cell.cs
@@ -1,22 +1,42 @@
+using System;
 using UnityEngine;
+using UnityEngine.tvOS;
 
-public enum State
-{
-    Hover,
-    Clicked,
-    Revealed
-}
+public enum State { Hover, Clicked, Revealed }
 
 public class Cell : MonoBehaviour
 {
+    private State _state;
+    [SerializeField] private GameObject _hoverBorders;
+    
+    private void OnMouseOver()
+    {
+        SetState(State.Hover);
+    }
+
+    private void OnMouseExit()
+    {
+    }
+
+    private void SetState(State state)
+    {
+        _state = state;
+        HandleStates();
+    }
+    
     private void HandleStates()
     {
-        switch (State)
+        switch (_state)
         {
-            case (State.Hover)
-            {
-                
-            }
+            case State.Hover:
+                _hoverBorders.SetActive(true);
+                break;
+            case State.Clicked:
+                break;
+            case State.Revealed:
+                break;
+            default:
+                Debug.Log("Default");
         }
     }
 }

99a0924dd5a2e52f5d007c201d05a50b109f5b99
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-02-15 08:12:19 +0200

    Created a cell prefab

---
+++ Assets/_Scripts/Cell.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public enum State
+{
+    Hover,
+    Clicked,
+    Revealed
+}
+
+public class Cell : MonoBehaviour
+{
+    private void HandleStates()
+    {
+        switch (State)
+        {
+            case (State.Hover)
+            {
+                
+            }
+        }
+    }
+}

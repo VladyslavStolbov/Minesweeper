cc369fbee7181781d60377bb7b817f2e47c9ca34
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-02-22 16:44:11 +0200

    Added some base method for Grid Class

--- Assets/_Scripts/Grid.cs
+++ Assets/_Scripts/Grid.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
-public class Grid
+public class Grid<TGridObject>
 {
     private int _width;
     private int _height;
     private float _cellSize;
     private Vector3 _originPosition;
     
-    private int[,] _grid;
+    private TGridObject[,] _grid;
     
     public Grid(int width, int height, float cellSize, Vector3 originPosition)
     {
@@ -18,9 +19,40 @@ public class Grid
         this._cellSize = cellSize;
         this._originPosition = originPosition;
         
-        this._grid = new int[_width, _height];
+        this._grid = new TGridObject[_width, _height];
     }
 
+    public void SetValue(int x, int y, TGridObject value)
+    {
+        if (x >= 0 && y >= 0 && x < _width && y < _height)
+        {
+            _grid[x, y] = value;
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, TGridObject value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public TGridObject GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < _width && y < _height)
+        {
+            return _grid[x, y];
+        }
+        return default;
+    }
+
+    public TGridObject GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+    
     private Vector3 GetWorldPosition(int x, int y)
     {
         return new Vector3(x, y) * _cellSize + _originPosition;
@@ -31,4 +63,4 @@ public class Grid
         x = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
         y = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
     }
-} 
+}

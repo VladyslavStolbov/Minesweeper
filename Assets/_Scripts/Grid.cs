8ce44c52827eadd4e256c0465a971524d18b6f39
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-02-22 07:58:44 +0200

    Scratch some lines of code

--- Assets/_Scripts/Grid.cs
+++ Assets/_Scripts/Grid.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public class Grid
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _width;
+    private int _height;
+    private float _cellSize;
+    private Vector3 _originPosition;
+    
+    private int[,] _grid;
+    
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
     {
+        this._width = width;
+        this._height = height;
+        this._cellSize = cellSize;
+        this._originPosition = originPosition;
         
+        this._grid = new int[_width, _height];
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 GetWorldPosition(int x, int y)
     {
-        
+        return new Vector3(x, y) * _cellSize + _originPosition;
     }
-}
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
+        y = Mathf.FloorToInt((worldPosition - _originPosition).y / _cellSize);
+    }
+} 

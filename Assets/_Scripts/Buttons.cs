6303de018cd4fe43a31a45beebc97429e7045da6
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2024-03-15 06:59:06 +0200

    Added a keyboard restart and exit input

--- Assets/_Scripts/Buttons.cs
+++ Assets/_Scripts/Buttons.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Exit();
+        }
+    }
+
     public void Play()
     {
         SceneManager.LoadSceneAsync("Game");
